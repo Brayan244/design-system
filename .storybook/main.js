75705c3a4e20e55fe6856ada06af82de3850f5d1
Juan Miguel Rubio <jmrubio42@gmail.com>
2020-01-27 14:28:07 -0600

    Configures prettier to work with eslint and stylelint

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../**/*.stories.js'],
+  stories: ['../**/*.stories.jsx'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
--- src/atoms/test.jsx
+++ src/atoms/test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './test.scss';
 
-const Test = () => (<div className="test" />);
+const Test = () => <div className="test" />;
 
 export default Test;
